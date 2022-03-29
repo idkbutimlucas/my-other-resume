743328486600c34d388034935755ecdad2e3da93
idkbutimlucas <76565606+idkbutimlucas@users.noreply.github.com>
2022-03-29 11:10:23 +0200

    🚧 buld and deploy

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import { useState } from 'react';
-import {storage} from './firebaseConf';
 import Projects from './components/Projects';
 
 function App() {
-  const [image, setImage] = useState('');
-  const [Url, setUrl] = useState('');
-  
-  const upload = () => {
-    if (image == null)
-      return;
-    setUrl("Getting Download Link...")
-  
-    // Sending File to Firebase Storage
-    storage.ref(`/images/${image.name}`).put(image)
-      .on("state_changed", alert("Bien envoyé !"), alert, () => {
-  
-        // Getting Download Link
-        storage.ref("images").child(image.name).getDownloadURL()
-          .then((url) => {
-            setUrl(url);
-          })
-      });
-  }
 
   return (
     <div className="App bg-neutral-200">
